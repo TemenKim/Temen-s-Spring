051685b393c1b352e7518d4bc86a91e5ee6341a7
Temen <96803342+TemenKim@users.noreply.github.com>
2022-07-19 14:57:02 +0900

    Add files via upload

---
+++ thymeleaf(con) - each(반복문).java
@@ -0,0 +1,66 @@
+package com.koreait.thymeleaf.basic;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.koreait.thymeleaf.data.User;
+
+@Controller
+@RequestMapping("/basic")
+public class BasicController {
+
+
+
+	@GetMapping("/each")
+	public String each(Model model) {
+//		List<User> list = new ArrayList<>();
+//		list.add(new User("userA", 10));
+//		list.add(new User("userB", 20));
+//		list.add(new User("userC", 30));
+//		model.addAttribute("users", list);
+		addUsers(model);
+		return "basic/each";
+	}
+
+	private void addUsers(Model model) {
+		List<User> list = new ArrayList<>();
+		list.add(new User("userA", 10));
+		list.add(new User("userB", 20));
+		list.add(new User("userC", 30));
+		model.addAttribute("users", list);
+	}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
